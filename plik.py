b0938e32140ea11f17017967cc669fdaae6c2751
BulletYY <thanusiak@student.agh.edu.pl>
2026-07-28 22:25:26 +0200

    added stuff to be cleaned

--- plik.py
+++ plik.py
@@ -1,13 +1,35 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from statsmodels.formula.api import ols 
 
 
 
-
-
-def flexible_fourier_form(data,N,criteria,vol_estimation,days):
+def flexible_fourier_form(data: pd.DataFrame, N: int, criteria: str, vol_estimation: str, days: list, plots: bool,session_thresholds:list):
     """ This function is a placeholder for the flexible Fourier form implementation. It is intended 
     to be developed further.    
     
     """
+    
+    data = data.query('TIME >= 90000 and TIME <= 165000') # wziąłem 9:05 do 16:50 dla starego kodu , Czy Wyrzucac dogrywke czy nie 
+
+    # log stopy zwrotu w obrębie dnia
+    data['log_return_intraday'] = (
+        data.groupby('DATE')['CLOSE']
+        .transform(lambda x: np.log(x).diff()) 
+    )
+
+    data = data.dropna(subset=['log_return_intraday'])
+    data['abs_zwroty'] = np.abs(data['log_return_intraday'])
+
+    #print("number of days:", data['DATE'].nunique())
+    #print("number of rows:", len(data))
+    #print(data.groupby('DATE').size().value_counts().sort_index())
+        
+        data['DATE'] =pd.to_datetime(data['DATE'],format='%Y%m%d')
+        
+    
+    data['dni_tygodnia'] =data['DATE'].dt.day_name()
         
     #
     N = 92 # ilosc 
@@ -16,16 +38,16 @@ def flexible_fourier_form(data,N,criteria,vol_estimation,days):
 
     N_2 = (N + 1) * (2*N + 1) / 6  #(N+1)*(N+2)/6 # bezposrednio z ksiazki prof gurg i wojtow
 
-    R_bar = fdt['log_return_intraday'].mean() ## R_bar = fdt['log_return_intraday'].mean() stare 
+    R_bar = data['log_return_intraday'].mean() ## R_bar = data['log_return_intraday'].mean() stare 
         
-    returns = fdt['log_return_intraday'] ## stare returns = fdt['log_return_intraday']
+    returns = data['log_return_intraday'] ## stare returns = data['log_return_intraday']
 
     sigma_hat = std_daily # 13110
 
 
-    fdt["R_bar_n"] = fdt.groupby("sesja")["log_return_intraday"].transform("mean")
+    data["R_bar_n"] = data.groupby("sesja")["log_return_intraday"].transform("mean")
 
-    returns_centered = returns - fdt["R_bar_n"]
+    returns_centered = returns - data["R_bar_n"]
 
 
 
@@ -33,6 +55,28 @@ def flexible_fourier_form(data,N,criteria,vol_estimation,days):
     response = 2*np.log(np.abs(returns_centered) / (sigma_hat / np.sqrt(N) ))  # oblcizenie zmiennej objasnianej y // returns -R_bar 
 
     
+        
+    aic_list = []
+    bic_list = []
+
+    # while i dodawac dp espression += dopóki p ==0 i odejmowac z kazdym while 
+    for p in range(1,11):
+        x=p
+        expression = f"y~linear+qube+Friday+Monday+Thursday+Tuesday+"
+        while p >0:
+            expression+= f"sinus_{p}"+"+"+f"cosinus_{p}+"
+            p-=1
+
+        expression =expression.rstrip("+")
+        print(expression)
+
+        model = ols(expression,data=data).fit()
+        aic_list.append([model.aic,x])
+        bic_list.append([model.bic,x])
+    
+    model2 = ols("y~linear+qube+sinus_1+cosinus_1+Wednesday+Monday+Thursday+Tuesday   ",data=binary_df).fit( cov_type="HAC",cov_kwds={"maxlags": int( 4*(binary_df.shape[0]/100)**(2/9)  ) })
+        
+        
     
     pass
 
