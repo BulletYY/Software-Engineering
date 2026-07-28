379a719c92635f7dc202e2e1ed14a028af07598a
BulletYY <thanusiak@student.agh.edu.pl>
2026-07-28 22:15:46 +0200

    Flexible Fourier Form start

--- plik.py
+++ plik.py
@@ -1,6 +1,41 @@
 
-x = 24
 
-y = 20 +21
 
-print("hello world",x+x,y+y)
+
+
+def flexible_fourier_form(data,N,criteria,vol_estimation,days):
+    """ This function is a placeholder for the flexible Fourier form implementation. It is intended 
+    to be developed further.    
+    
+    """
+        
+    #
+    N = 92 # ilosc 
+
+    N_1 = (N+1) /2  # bezposrednio z ksiazki prof gurg i wojtow
+
+    N_2 = (N + 1) * (2*N + 1) / 6  #(N+1)*(N+2)/6 # bezposrednio z ksiazki prof gurg i wojtow
+
+    R_bar = fdt['log_return_intraday'].mean() ## R_bar = fdt['log_return_intraday'].mean() stare 
+        
+    returns = fdt['log_return_intraday'] ## stare returns = fdt['log_return_intraday']
+
+    sigma_hat = std_daily # 13110
+
+
+    fdt["R_bar_n"] = fdt.groupby("sesja")["log_return_intraday"].transform("mean")
+
+    returns_centered = returns - fdt["R_bar_n"]
+
+
+
+
+    response = 2*np.log(np.abs(returns_centered) / (sigma_hat / np.sqrt(N) ))  # oblcizenie zmiennej objasnianej y // returns -R_bar 
+
+    
+    
+    pass
+
+
+
+
